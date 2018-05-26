ecd7414bb861c60811e39689e6dca6bac4449403
chroda <chroda@chroda.com.br>
2018-05-26 11:15:17 -0300

    doing

--- your-first-pwapp-master/work/scripts/app.js
+++ your-first-pwapp-master/work/scripts/app.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,7 @@
 
 (function() {
   'use strict';
-  
+
   var app = {
     isLoading: true,
     visibleCards: {},
@@ -26,24 +26,24 @@
     addDialog: document.querySelector('.dialog-container'),
     daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   };
-  
-  
+
+
   /*****************************************************************************
   *
   * Event listeners for UI elements
   *
   ****************************************************************************/
-  
+
   document.getElementById('butRefresh').addEventListener('click', function() {
     // Refresh all of the forecasts
     app.updateForecasts();
   });
-  
+
   document.getElementById('butAdd').addEventListener('click', function() {
     // Open/show the add new city dialog
     app.toggleAddDialog(true);
   });
-  
+
   document.getElementById('butAddCity').addEventListener('click', function() {
     // Add the newly selected city
     var select = document.getElementById('selectCityToAdd');
@@ -58,19 +58,19 @@
     app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
-  
+
   document.getElementById('butAddCancel').addEventListener('click', function() {
     // Close the add new city dialog
     app.toggleAddDialog(false);
   });
-  
-  
+
+
   /*****************************************************************************
   *
   * Methods to update/refresh the UI
   *
   ****************************************************************************/
-  
+
   // Toggles the visibility of the add new city dialog.
   app.toggleAddDialog = function(visible) {
     if (visible) {
@@ -79,7 +79,7 @@
       app.addDialog.classList.remove('dialog-container--visible');
     }
   };
-  
+
   // Updates a weather card with the latest weather forecast. If the card
   // doesn't already exist, it's cloned from the template.
   app.updateForecastCard = function(data) {
@@ -89,7 +89,7 @@
     var current = data.channel.item.condition;
     var humidity = data.channel.atmosphere.humidity;
     var wind = data.channel.wind;
-    
+
     var card = app.visibleCards[data.key];
     if (!card) {
       card = app.cardTemplate.cloneNode(true);
@@ -99,7 +99,7 @@
       app.container.appendChild(card);
       app.visibleCards[data.key] = card;
     }
-    
+
     // Verifies the data provide is newer than what's already visible
     // on the card, if it's not bail, if it is, continue and update the
     // time saved in the card
@@ -113,7 +113,7 @@
       }
     }
     cardLastUpdatedElem.textContent = data.created;
-    
+
     card.querySelector('.description').textContent = current.text;
     card.querySelector('.date').textContent = current.date;
     card.querySelector('.current .icon').classList.add(app.getIconClass(current.code));
@@ -148,14 +148,14 @@
       app.isLoading = false;
     }
   };
-  
-  
+
+
   /*****************************************************************************
   *
   * Methods for dealing with the model
   *
   ****************************************************************************/
-  
+
   /*
   * Gets a forecast for a specific city and updates the card with the data.
   * getForecast() first checks if the weather data is in the cache. If so,
@@ -169,7 +169,7 @@
     var url = 'https://query.yahooapis.com/v1/public/yql?format=json&q=' +
     statement;
     // TODO add cache logic here
-    
+
     // Fetch the latest data.
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
@@ -190,7 +190,7 @@
     request.open('GET', url);
     request.send();
   };
-  
+
   // Iterate all of the cards and attempt to get the latest forecast data
   app.updateForecasts = function() {
     var keys = Object.keys(app.visibleCards);
@@ -198,14 +198,14 @@
       app.getForecast(key);
     });
   };
-  
+
   // TODO add saveSelectedCities function here
   // Save list of cities to localStorage.
   app.saveSelectedCities = function() {
     var selectedCities = JSON.stringify(app.selectedCities);
     localStorage.selectedCities = selectedCities;
   };
-  
+
   app.getIconClass = function(weatherCode) {
     // Weather codes: https://developer.yahoo.com/weather/documentation.html#codes
     weatherCode = parseInt(weatherCode);
@@ -269,7 +269,7 @@
       return 'partly-cloudy-day';
     }
   };
-  
+
   /*
   * Fake weather data that is presented when the user first uses the app,
   * or when the user has not saved any cities. See startup code for more
@@ -312,18 +312,18 @@
   };
   // TODO uncomment line below to test app with fake data
   app.updateForecastCard(initialWeatherForecast);
-  
+
   // TODO add startup code here
   /************************************************************************
-   *
-   * Code required to start the app
-   *
-   * NOTE: To simplify this codelab, we've used localStorage.
-   *   localStorage is a synchronous API and has serious performance
-   *   implications. It should not be used in production applications!
-   *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
-   *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
-   ************************************************************************/
+  *
+  * Code required to start the app
+  *
+  * NOTE: To simplify this codelab, we've used localStorage.
+  *   localStorage is a synchronous API and has serious performance
+  *   implications. It should not be used in production applications!
+  *   Instead, check out IDB (https://www.npmjs.com/package/idb) or
+  *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
+  ************************************************************************/
   app.selectedCities = localStorage.selectedCities;
   if (app.selectedCities) {
     app.selectedCities = JSON.parse(app.selectedCities);
@@ -332,21 +332,21 @@
     });
   } else {
     /* The user is using the app for the first time, or the user has not
-     * saved any cities, so show the user some fake data. A real app in this
-     * scenario could guess the user's location via IP lookup and then inject
-     * that data into the page.
-     */
+    * saved any cities, so show the user some fake data. A real app in this
+    * scenario could guess the user's location via IP lookup and then inject
+    * that data into the page.
+    */
     app.updateForecastCard(initialWeatherForecast);
     app.selectedCities = [
       {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
     ];
     app.saveSelectedCities();
   }
-  
+
   // TODO add service worker code here
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+    .register('./service-worker.js')
+    .then(function() { console.log('Service Worker Registered'); });
   }
 })();
